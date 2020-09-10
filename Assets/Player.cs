a919efb690b69df7faa416cd9b1ed446d742c1af
juliotmorais <juliotmorais@gmail.com>
2020-09-10 18:52:41 +0900

    added art and sound assets, started player object, added horiztontal movement via keyboard

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D myRigidBody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidBody2D.velocity = new Vector2(moveSpeed*Input.GetAxis("Horizontal"), myRigidBody2D.velocity.y);
+    }
+}
