73924abfab46d9c9bc6b7b4eae0256b2ecb211fd
juliotmorais <juliotmorais@gmail.com>
2020-09-16 12:48:41 +0900

    no more unlimited jumping

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,10 @@ public class Player : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D myRigidBody2D;
     public float jumpForce;
+    public bool isGrounded;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +22,14 @@ public class Player : MonoBehaviour
     {
         myRigidBody2D.velocity = new Vector2(moveSpeed*Input.GetAxis("Horizontal"), myRigidBody2D.velocity.y);
 
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position,.2f,whatIsGround);
+
         if (Input.GetButtonDown("Jump"))
         {
-            myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x, jumpForce);
+            if (isGrounded)
+            {
+                myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x, jumpForce);
+            }
         }
     }
 }
